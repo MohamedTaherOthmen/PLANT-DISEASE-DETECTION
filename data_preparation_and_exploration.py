c91542208dfec4a262df9f1466a126126aa35820
Mohamed Taher Othmen <mohamedtaher.othman@gmail.com>
2025-12-03 03:08:40 +0100

    pre processing adjusment

---
+++ data_preparation_and_exploration.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, models, preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+
+
